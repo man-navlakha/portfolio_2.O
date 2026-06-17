52c78a1b90cd948330a220478d6b495775e4c805
man-navlakha <mannavlakha1021@gmail.com>
2026-06-17 19:54:28 +0530

    added favicons

--- app/layout.js
+++ app/layout.js
@@ -87,9 +87,16 @@ export const metadata = {
     },
   },
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png',
+    other: [
+      { rel: 'android-chrome', url: '/android-chrome-192x192.png' },
+    ],
   },
 };
 
